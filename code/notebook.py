98a4e3d53027f91d678ca8699363cb27427a68fe
leoposc <schmid.leopold@web.de>
2023-06-04 17:17:53 +0200

    synchronise

--- code/notebook.py
+++ code/notebook.py
@@ -3,8 +3,8 @@ from db import DBManager
 
 # fetch data from servers
 manager = DBManager()
-# manager.fetch_solar_data(10, "2022-04-01", "2022-04-30")
-manager.fetch_weather_data("applewood", "2022-04-01", "2022-04-30")
+manager.fetch_solar_data(10, "2022-05-01", "2022-05-31")
+# manager.fetch_weather_data("applewood", "2022-05-01", "2022-05-31")
 
 
 # %%
@@ -13,8 +13,8 @@ from scikit import ScikitManager
 
 sci = ScikitManager(location='applewood', solarsystem_id=10)
 sci.get_data()
-# sci.compare_similar_radiation(470, 550)
-sci.filter_low_radiation()
+sci.compare_similar_radiation(470, 550)
+# sci.filter_low_radiation()
 #%%
 
 # sci.split_data(0.99)
